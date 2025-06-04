c14959b524865fe737a97de80d5300f043aedae7
luis alberto Alvarez <luisslaycore@gmail.com>
2025-06-04 03:06:20 -0600

    logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,8 +2,11 @@
 
 export default function Logo() {
   return (
-    <img src='/logo.svg' alt='Logotipo Uptask' className=" md:w-full " >
-      
-    </img>
+    <header className="flex flex-row justify-center items-center">
+     <img src='/logo1.png' alt='Logotipo Uptask' className="w-28 md:w-44 " >   
+     </img>
+     <p className="text-4xl  md:text-9xl text-white font-medium ">Task</p>
+    </header>
+   
   )
 }
